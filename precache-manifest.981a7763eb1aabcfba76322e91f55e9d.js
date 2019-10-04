1446a2b111705719ee6526dd52db59ecbdb80814
kana-sama <andrew4chrome@gmail.com>
2019-10-04 22:03:52 +0300

    Updates

---
+++ precache-manifest.981a7763eb1aabcfba76322e91f55e9d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3180e63c786742b13c146e7d0d930fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa8266b72c040434ca30",
+    "url": "/static/js/2.a77f7bb0.chunk.js"
+  },
+  {
+    "revision": "aa1e4320bd3f0ecc3c5a",
+    "url": "/static/js/main.57b20857.chunk.js"
+  },
+  {
+    "revision": "d49968e7489d16ef4fe6",
+    "url": "/static/js/runtime-main.28066eb0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/edita/precache-manifest.cf4bca650bdbcdb14a71563c8ecbde50.js"
+  "/precache-manifest.981a7763eb1aabcfba76322e91f55e9d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/edita/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
